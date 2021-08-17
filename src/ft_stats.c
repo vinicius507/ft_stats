33176c07cf4d73cf85c8859cd49306eb186bd333
Vinicius Gonçalves de Oliveira <vinicius@myriaddev.me>
2021-08-17 14:52:29 -0300

    feat: basic api structure is finished. marking for review

--- src/ft_stats.c
+++ src/ft_stats.c
@@ -12,12 +12,6 @@
 
 #include "ft_stats.h"
 
-static void	echo(struct mg_connection *c, struct mg_http_message *req)
-{
-	mg_http_reply(c, 200, "", "{ \"foo\": \"%s\" }\n", "bar");
-	(void)req;
-}
-
 static void	redirect(struct mg_connection *c, struct mg_http_message *req)
 {
 	mg_http_reply(c, 308, "location: /api/v1\r\n", "");
@@ -29,7 +23,6 @@ int	main(void)
 	struct s_api	ft_stats;
 
 	api_init(&ft_stats);
-	register_route(GET, API_V1, echo, &ft_stats);
 	register_route(GET, API_V1_, redirect, &ft_stats);
 	api_do(&ft_stats);
 	return (0);
