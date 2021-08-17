d46cebde05639e4d8384096f5e99f086a56bdad1
Vinicius Gonçalves de Oliveira <vinicius@myriaddev.me>
2021-08-17 14:25:20 -0300

    feat: listen on port 4242

--- src/api.c
+++ src/api.c
@@ -29,7 +29,7 @@ void	api_do(struct s_api *api)
 {
 	mg_log_set("3");
 	mg_mgr_init(&api->mgr);
-	mg_http_listen(&api->mgr, "http://0.0.0.0:8000", cb, api);
+	mg_http_listen(&api->mgr, "http://0.0.0.0:4242", cb, api);
 	while (1)
 		mg_mgr_poll(&api->mgr, 1000);
 	mg_mgr_free(&api->mgr);
