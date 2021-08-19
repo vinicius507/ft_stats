81c15dd6a512c746579a909d98a5079aff96951f
Vinicius Gonçalves de Oliveira <vinicius@myriaddev.me>
2021-08-19 15:14:49 -0300

    feat: parsing user data from intranet

--- src/get_user_data.c
+++ src/get_user_data.c
@@ -15,7 +15,8 @@
 void	get_user_data(struct mg_connection *c, struct mg_http_message *req)
 {
 	struct s_api	*api;
-	char			*user;
+	char			*query;
+	struct s_user	user;
 
 	api = (struct s_api *)c->fn_data;
 	auth_intra(api);
@@ -24,8 +25,9 @@ void	get_user_data(struct mg_connection *c, struct mg_http_message *req)
 		mg_http_reply(c, 500, "", "");
 		return ;
 	}
-	user = strndup(req->uri.ptr + 8, req->uri.len - 8);
-	get_user_data_intra(api, user);
+	query = strndup(req->uri.ptr + 8, req->uri.len - 8);
+	get_user_data_intra(api, query);
+	parse_user_data(api, &user);
 	if (api->res.status == 200)
 		mg_http_reply(c, 200, "", "%s", api->res.body);
 	else if (api->res.status == 404)
@@ -33,8 +35,8 @@ void	get_user_data(struct mg_connection *c, struct mg_http_message *req)
 	else if (api->res.status == 500)
 		mg_http_reply(c, 500, "", "");
 	else
-		mg_http_reply(c, 400, "", api->res.body);
-	free(user);
-	free((void *)api->res.body);
-	api->res.body = NULL;
+		mg_http_reply(c, 400, "", "");
+	free(query);
+	free((void *)api->res.body), api->res.body = NULL;
+	free((void *)user._str);
 }
