7fefe670fb47f00306b9c47e38df43d9d8fde3fe
Vinicius Gonçalves de Oliveira <vinicius@myriaddev.me>
2021-08-20 16:00:03 -0300

    fix: now sigterm handler really kills the process

--- src/ft_stats.c
+++ src/ft_stats.c
@@ -12,31 +12,10 @@
 
 #include "ft_stats.h"
 
-static struct s_api	*g_api = NULL;
-
-static void	_sig_term_handler(int signum)
-{
-	mongoc_cleanup();
-	mg_mgr_free(&g_api->mgr);
-	exit(EXIT_SUCCESS);
-	(void)signum;
-}
-
-void	catch_sigterm(void)
-{
-	static struct sigaction	_sigact;
-
-	memset(&_sigact, 0, sizeof(_sigact));
-	_sigact.sa_handler = _sig_term_handler;
-	sigaction(SIGTERM, &_sigact, NULL);
-}
-
 int	main(void)
 {
 	struct s_api	api;
 
-	g_api = &api;
-	catch_sigterm();
 	api_init(&api);
 	register_route(GET, API_V1_, redirect, &api);
 	register_route(GET, API_V1_USER, get_user_data, &api);
